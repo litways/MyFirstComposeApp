646470d727727172d45592a2d2bf35a99f2ac9c9
litways <lukui301@gmail.com>
2026-01-21 18:54:22 +0800

    Add material icons dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,9 +10,7 @@ plugins {
 
 android {
     namespace = "com.example.myfirstcomposeapp"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.myfirstcomposeapp"
@@ -55,6 +53,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.compose.material:material-icons-extended")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
