72b4a93c270d05cae4c7dbd8944db7bc5faea87d
litways <lukui301@gmail.com>
2026-01-21 18:44:41 +0800

    Fix compileSdk configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,9 +10,7 @@ plugins {
 
 android {
     namespace = "com.example.myfirstcomposeapp"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.myfirstcomposeapp"
